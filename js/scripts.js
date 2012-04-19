c4a072502e27d969d654bbceca0e5be203725c8a
Jakob Anderson <fictionisfiction@gmail.com>
2012-04-19 09:00:54 -0600

    Morning Commit. Finished Assignment 1 and got it to validate to XHTML1.1 Yick!

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(window).load(function() {
+
+	$('.hero-unit img').fadeIn();
+	$('.well').slideDown();
+	$('.nav-collapse li a')
+
+	//header contact icons animation
+   $(".nav-collapse li a").hover(
+	  function () {
+	    $(this).animate({
+	    opacity: 1,
+	    top: '-5px'
+	  },{queue:false,duration:200});
+	  },
+	  function () {
+	    $(this).animate({
+	    opacity: 0.7,
+	    top: '0px'
+	  },{queue:false,duration:200});
+	  }
+	);
+
+});
