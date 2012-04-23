45af3000f5e2c4090dd9276ddbab2411afc43d63
Jakob Anderson <fictionisfiction@gmail.com>
2012-04-22 22:15:23 -0600

    small patch

--- post.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $json = file_get_contents('scifi_results.json');
-    $data = json_decode($json);
-    $data[] = $_POST['scifi_form'];
-    file_put_contents('scifi_results.json', json_encode($data);
-?>
