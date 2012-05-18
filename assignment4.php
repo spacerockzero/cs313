11e825551709cc45918c4b07b00a959a698f17c3
Jakob Anderson <fictionisfiction@gmail.com>
2012-05-17 21:47:44 -0600

    beginning ajax process for assignment 4

--- assignment4.php
+++ assignment4.php
@@ -1,27 +1,3 @@
-<?php 
-
-  $hostName = '157.201.194.254';
-  $userName = 'skabone';
-  $password = '';
-
-  if (!($db=mysql_connect($hostName, $userName, $password))) {
-    print 'cannot connect msg';
-  }
-  else
-  {
-    print 'successful connection<br/>';
-  }
-
-  $database = 'skabone';
-
-  if(!(mysql_select_db($database))) {
-    //print can't connect
-    print 'cannot select db<br/>';
-  }
-
-
-
-?>
 <?php include('modules/header-top.php');?>
 
     <title>Jakob Anderson >> CS313 [Web Engineering II] | Assignment 4</title>
@@ -35,37 +11,13 @@
             <h1 id="fittext1">Assignment 4</h1>
             <h3>Input SQL query</h3>
             <input type="textfield" value="type SQL query here"></textfield>
-
+            <input type="button" value="submit query" onclick="ajax_query()"/>
             <h3>Results:</h3>
-            <p>
-              <?php 
-
-                $query = "SELECT * FROM students";
-
-                $result = mysql_query($query);
-                  
-                if($result == false) 
-                { 
-                   user_error("Query failed: " . mysql_error() . "<br />\n$query"); 
-                } 
-                elseif(mysql_num_rows($result) == 0) 
-                { 
-                   echo "<p>Sorry, no rows were returned by your query.</p>\n"; 
-                } 
-                else 
-                { 
-                   while($query_row = mysql_fetch_assoc($result)) 
-                   { 
-                      foreach($query_row as $key => $value) 
-                      { 
-                         echo "$key: $value<br />\n"; 
-                      } 
-                      echo "<br />\n"; 
-                   } 
-                } ?>
-            </p>
+            <p></p>
 
 <?php include('modules/footer.php');?>        
-    
+    <script type="text/javascript">
+      
+    </script>
   </body>
 </html>
