3bf3d8b95a5055d67d916d4a3036ddcfd790953d
Jakob Anderson <fictionisfiction@gmail.com>
2012-05-20 19:37:58 -0600

    small patch

--- assignment5.php
+++ assignment5.php
@@ -8,8 +8,40 @@
 
 <?php include('modules/frame-top.php');?>
 
-            <h1 id="fittext1">Assignment 5</h1>
-            <p></p>
+            <h1>Assignment 5</h1>
+            <div id="students_table">
+            <h4>Students</h4>
+            <table>
+            	<thead>
+            		<tr>
+            			<th>StudentId</th>
+            			<th>FirstName</th>
+            			<th>LastName</th>
+            			<th>Major</th>
+            			<th>Birthdate</th>
+            			<th>Gender</th>
+            			<th>City</th>
+            			<th>State</th>
+            			<th>Edit</th>
+            		</tr>
+            	</thead>
+            	<tfoot>
+            		Table Foot
+            	</tfoot>
+            	<tbody>
+            		<tr>
+            			<td>1</td>
+            			<td>Albin</td>
+            			<td>Gustavstrohm</td>
+            			<td>1001</td>
+            			<td>1980-06-01</td>
+            			<td>M</td>
+            			<td>Tacoma</td>
+            			<td>WA</td>
+            		</tr>
+            	</tbody>
+            </table>
+            </div>
 
 <?php include('modules/footer.php');?>        
     
