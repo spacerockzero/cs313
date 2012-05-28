76ba42f2babf39930b3a719741d0c2e255931654
Jakob Anderson <fictionisfiction@gmail.com>
2012-05-27 21:41:52 -0600

    more work on assignment 5

--- assignment5.php
+++ assignment5.php
@@ -1,17 +1,20 @@
 <?php
   //Home OSX MAMP / Laptop LAMPP DB setup
-  // $hostName = 'localhost';
-  // $userName = 'root';
-  // $password = '@Nd3r50n15th3b055';
+  $hostName = 'localhost';
+  $userName = 'root';
+  $password = '@Nd3r50n15th3b055';
 
   //Jordan Campus DB Setup
-  $hostName = '157.201.194.254';
-  $userName = 'skabone';
-  $password = '';
+  // $hostName = '157.201.194.254';
+  // $userName = 'skabone';
+  // $password = '';
 
   $queryGood = false;
   $addStudent = false;
+  $editStudent = false;
   $removeStudent = false;
+  global $studentInfo;
+  $studentInfo = array();
 
   if (!($db=mysql_connect($hostName, $userName, $password))) {
     print 'cannot connect msg';
@@ -40,7 +43,11 @@ if (isset($_GET['action'])) {
 
   //edit student routing
   if ($_GET['action'] === 'edit') {
-
+    $id = $_GET['id'];
+    print "inside edit student";
+    print "<br/>id = ".$id;
+    $editStudent = true;
+    selectStudent($id);
   }
 
   //remove student routing
@@ -95,18 +102,52 @@ if (isset($_POST['addStudentSubmit'])) {
 }
 
 function removeStudent($id){
-      $removeQuery = "DELETE FROM students WHERE StudentId = '$id'";
+  $removeQuery = "DELETE FROM students WHERE StudentId = '$id'";
 
-      $removeResult = mysql_query($removeQuery);
+  $removeResult = mysql_query($removeQuery);
 
-      if($removeResult == false) { 
-        user_error("Query failed, yo: " . mysql_error() . "<br />\n$removeResult"); 
-      }  
-      else { 
-        //successfully retrieved row results
-        print "successfully removed record";
-      }
-    }
+  if($removeResult == false) { 
+    user_error("Query failed, yo: " . mysql_error() . "<br />\n$removeResult"); 
+  }  
+  else { 
+    //successfully retrieved row results
+    print "successfully removed record";
+  }
+}
+
+function selectStudent($id){
+
+  $query = "SELECT * FROM students where StudentId = '$id'";
+
+  $result = mysql_query($query);
+    
+  if($result == false) { 
+     user_error("Query failed: " . mysql_error() . "<br />\n$query"); 
+  } 
+  elseif(mysql_num_rows($result) == 0) { 
+     echo "<p>Sorry, no rows were returned by your query.</p>\n"; 
+  } 
+  else { 
+    //successfully retrieved row results
+    
+    print "<br />\n Successfully selected student with id = ".$id;
+    
+    // while ($row = mysql_fetch_assoc($result)) {
+    //     echo $row['FirstName'];
+    //     echo $row['LastName'];
+    //     echo $row['MajorCode'];
+    //     echo $row['Birthdate'];
+    //     echo $row['Gender'];
+    //     echo $row['City'];
+    //     echo $row['State'];
+    // }
+
+    $studentInfo = mysql_fetch_assoc($result);
+    // print_r($student);
+    echo $studentInfo['FirstName'];
+  }
+
+}
 
 $query = "SELECT * FROM students ORDER BY StudentId DESC";
 
@@ -141,11 +182,11 @@ else {
 
               <?php 
               //check if add student button is needed
-              if($addStudent == false) { ?>
+              if($addStudent == false && $editStudent == false) { ?>
               <a href="?action=addStudent" id="addStudentBtn" class="btn btn-primary btn-large"><strong>+</strong> Add Student</a>
               <?php 
               //print all forms needed
-              } else { ?>
+              } if ($addStudent == true){ ?>
               <div id="addStudentDiv" class="cf">
 
                 <form id="addStudent" name="addStudent" action="assignment5.php" method="post">
@@ -193,10 +234,59 @@ else {
                   <a id="addStudentCancel" name="addStudentCancel" href="assignment5.php" class="btn btn-danger btn-large" type="button" >Cancel</a>
                   <input id="addStudentSubmit" name="addStudentSubmit" class="btn btn-primary btn-large" type="submit" value="+ Add New Student" >
 
-
                 </form>
               </div>
 
+              <?php } if ( $editStudent == true ){ ?>
+              <div id="addStudentDiv" class="cf">
+                <h3>Edit Student  <?php echo $studentInfo['FirstName']; ?></h3>
+                <form id="editStudent" name="editStudent" action="assignment5.php" method="post">
+                  
+                  <label for="FirstName"> First Name</label>
+                  <input type="text" name="FirstName" id="FirstName" />
+                  
+                  <label for="LastName"> Last Name</label>
+                  <input type="text" name="LastName" id="LastName" />
+
+                  <br/>
+                  <label for="MajorCode"> MajorCode</label>
+                  <select name="MajorCode" id="MajorCode">
+                    <option value="1001">1001 (B.S. in Biology)</option>
+                    <option value="1002">1002 (B.S. in Psychology)</option>
+                    <option value="1003">1003 (B.S. in Architecture)</option>
+                    <option value="1004">1004 (B.S. in Computer Science)</option>
+                    <option value="1005">1005 (B.S. in Mechanical Engineering)</option>
+                    <option value="1006">1006 (B.S. in Automotive Technology)</option>
+                    <option value="1007">1007 (B.S. in Computer Information Technology)</option>
+                    <option value="1008">1008 (B.S. in Business Management)</option>
+                    <option value="1009">1009 (B.S. in Art)</option>
+                    <option value="1010">1010 (B.S. in Interior Design)</option>
+                  </select>
+
+                  <label for="Birthdate"> Birthdate</label>
+                  <input name="Birthdate" type="text" class="span2" id="dp2" value="1985-01-01">
+
+
+                  <br/>
+                  <label for="Gender"> Gender</label>
+                  <select name="Gender" id="Gender">
+                    <option value="M">M</option>
+                    <option value="F">F</option>
+                  </select>
+
+                  <br/>
+                  <label for="City"> City</label>
+                  <input type="text" name="City" id="City" />
+
+                  <label for="State"> State</label>
+                  <input type="text" name="State" id="State" />
+                  <br/>
+                  <br/>
+                  <a id="editStudentCancel" name="editStudentCancel" href="assignment5.php" class="btn btn-danger btn-large" type="button" >Cancel</a>
+                  <input id="editStudentSubmit" name="editStudentSubmit" class="btn btn-primary btn-large" type="submit" value="Submit Changes" >
+
+                </form>
+              </div>
               <?php } ?>
               <h3>Students</h3>
               <?php 
